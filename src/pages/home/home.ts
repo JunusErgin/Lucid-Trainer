e22990b6aa072fdd51ed066115dd74eadfe4689c
Junus Ergin <junus91@googlemail.com>
2016-12-06 17:16:42 +0100

    Adjusted loading and advertisement

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,78 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 import { TranslationService } from '../../services/translation.service';
+
+declare var AdMob: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  checkingDream:boolean = false;
+  private admobId: any;
+  checkingDream: boolean = false;
 
   constructor(public navCtrl: NavController,
+              private platform: Platform,
               public translationService: TranslationService) {
-    console.log('Loaded translation Service', translationService);
+    this.platform = platform;
+    console.log('Loaded services', translationService, this.platform);
+
+    this.admobId = {
+      banner: 'ca-app-pub-8247738756840583/9268330958',
+      interstitial: 'ca-app-pub-8247738756840583/5499109356'
+    };
+
+    this.createBanner();
   }
+
 
   public checkDreamState(): void {
     this.checkingDream = true;
+    let self: HomePage = this;
     setTimeout(() => {
-      this.checkingDream = false;
+      self.checkingDream = false;
+      self.showInterstitial();
     }, 2000);
+  }
+
+  private showInterstitial() {
+    this.platform.ready().then(() => {
+      if (typeof AdMob !== 'undefined') {
+        AdMob.prepareInterstitial({
+          adId: this.admobId.interstitial,
+          autoShow: true
+        });
+      }
+    });
+  }
+
+
+  private createBanner() {
+    this.platform.ready().then(() => {
+      if (typeof AdMob !== 'undefined') {
+        console.log('Create banner');
+        AdMob.createBanner({
+          adId: this.admobId.banner,
+          autoShow: true
+        });
+        this.showBanner('bottom');
+      }
+    });
+  }
+
+  private showBanner(position: string) {
+    this.platform.ready().then(() => {
+      console.log('Checking AdMob', AdMob, !!AdMob);
+      if (!!AdMob) {
+        var positionMap = {
+          "bottom": AdMob.AD_POSITION.BOTTOM_CENTER,
+          "top": AdMob.AD_POSITION.TOP_CENTER
+        };
+        AdMob.showBanner(positionMap[position.toLowerCase()]);
+      }
+    });
   }
 
 }
